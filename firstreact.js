32d6e32e5cb9dc9917b76a2c6a1c9b7516871299
Jyotiverma706 <81759365+Jyotiverma706@users.noreply.github.com>
2021-04-11 19:47:26 +0530

    Update firstreact.js

--- firstreact.js
+++ firstreact.js
@@ -1,31 +1,34 @@
-import './App.css';
-
-function App() {
-  const fur=[
-    {
-    "name":"designing sofa"},
-    {"price": 2000}]
-    const list=fur.map(abc=><h4>{abc.name}{abc.price}</h4>)
-    console.log(list)
-  return (
-   <div>
-     <h1>Furniture Shop</h1>
-     {list}
-   </div>
-  );
-  
-}
-
-export default App;
 
 import './App.css';
 
 function App() {
-  const fur=[
+  const fur = [
     {
-    "name":"designing sofa"},
-    {"price": 2000}]
-    const list=fur.map(abc=><h4>{abc.name}{abc.price}</h4>)
+      "name": "Furniture Cafe Wooden Intersecting Wall Mounted Shelf"
+    },
+    { "price": 2000 },
+    {'src':'https://www.amazon.in/Furniture-Wooden-Intersecting-Shelves-Living/dp/B07F5T7B71/ref=sr_1_5?crid=14LANL9YD4DNK&dchild=1&keywords=furnitures+for+home&qid=1618149451&sprefix=furniture%2Caps%2C806&sr=8-5'},
+    {
+      "name": "Scuzo Microfiber Chair Pad Cushion, 48 x 16 Inches, Blue , Pack of 1designing sofa"
+    },
+    { "price": 2000 },
+    {'src':'https://www.amazon.in/Exclusive-Relaxing-Cushion-Rocking-Outdoor/dp/B08G88CZQJ/ref=sr_1_20?crid=14LANL9YD4DNK&dchild=1&keywords=furnitures+for+home&qid=1618149451&sprefix=furniture%2Caps%2C806&sr=8-20'},
+      {
+      "name": "designing sofa"
+    },
+    { "price": 2000 },
+    {'src':'https://www.amazon.in/Furniture-Wooden-Intersecting-Shelves-Living/dp/B07F5T7B71/ref=sr_1_5?crid=14LANL9YD4DNK&dchild=1&keywords=furnitures+for+home&qid=1618149451&sprefix=furniture%2Caps%2C806&sr=8-5'},
+        {
+      "name": "designing sofa"
+    },
+    { "price": 2000 },
+    {'src':'https://www.amazon.in/Furniture-Wooden-Intersecting-Shelves-Living/dp/B07F5T7B71/ref=sr_1_5?crid=14LANL9YD4DNK&dchild=1&keywords=furnitures+for+home&qid=1618149451&sprefix=furniture%2Caps%2C806&sr=8-5'},
+          {
+      "name": "designing sofa"
+    },
+    { "price": 2000 }
+    {'src':'https://www.amazon.in/Furniture-Wooden-Intersecting-Shelves-Living/dp/B07F5T7B71/ref=sr_1_5?crid=14LANL9YD4DNK&dchild=1&keywords=furnitures+for+home&qid=1618149451&sprefix=furniture%2Caps%2C806&sr=8-5'},]
+    const list=fur.map(abc=><div>{abc.src}<h4>{abc.name}{abc.price}</h4></div>)
     console.log(list)
   return (
    <div>
